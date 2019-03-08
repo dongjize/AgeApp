ec6bb4bcaec394169ecfda65fa429743bfcf4658
mason <ms.catmissyou@gmail.com>
2019-03-08 15:43:06 +0800

    extractAge done

--- trainingModel/extractAge.py
+++ trainingModel/extractAge.py
@@ -1,5 +1,4 @@
 import os
-import re
 
 """
 Extract age information from jpg files and generate label files.
@@ -9,31 +8,34 @@ Extract age information from jpg files and generate label files.
 
 PROJECT_PATH = os.path.dirname(os.path.dirname(__file__))
 
-path = PROJECT_PATH + "/trainingModel/utkface-new/test/"
+img_path = PROJECT_PATH + "/trainingModel/utkface-new/test/"
 
 
-img = []
-label = []
+def extract_age(path):
+    img = []
+    img_path = []
+    age_label = []
 
-for file in os.listdir(path):
-    print(file)
-    file_path = os.path.join(path, file)
-    print(file_path)
-    for imgs in os.listdir(file_path):
-        print(imgs)
+    for file in os.listdir(path):
+        # print(file)
+        file_path = os.path.join(path, file)
+        # print(file_path)
+        if os.path.isfile(file_path):
+            img += [file]
+            img_path += [file_path]
+        # print(img)
 
+    num = len(img)
+    for i in range(num):
+        imgs = img[i]
+        age = imgs.split("_")[0]
+        age_label += [age]
+    assert len(img) == len(age_label)
 
-
-    # if os.path.isdir(file_img):
-    # img = img + os.listdir(file_img)
-num = len(img)
-for i in range(num):
-    imgs = img[i]
-    age = imgs.split("_")[0]
-    label = label + [age]
-assert len(img) == len(label)
-
-print("trained img:", len(img), "......trained label", len(label))
+    print(img)
+    print(age_label)
+    print(img_path)
+    print("Train imgs:", len(img), "......Train labels", len(age_label))
 
 #
 # # change file name
@@ -48,31 +50,8 @@ print("trained img:", len(img), "......trained label", len(label))
 #     print(".....done.......")
 
 
-# generate label
-def generate_label(path):
-    img = []
-    label = []
-    for file in os.listdir(path):
-        file_img = os.path.join(path, file)
-        if os.path.isdir(file_img):
-            img = img + os.listdir(file_img)
-    num = len(img)
-    for i in range(num):
-        imgs = img[i]
-        name1 = imgs.split(".")[-2]  # get elements before .jpg
-        name2 = name1.split('%')[-1]  # get label
-        label = label + [int(name2)]
-    assert len(img) == len(label)
+if __name__ == '__main__':
 
-    print("train img:", len(img), "......train label", len(label))
+    extract_age(img_path)
 
-
-
-
-
-
-# if __name__ == '__main__':
-#
-#     # generate_label(path)
-#
-#     rename_file(path)
+    # rename_file(path)
